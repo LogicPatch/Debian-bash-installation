794431682bccb28a3b175aee5e162cd3356122a1
Oliver Prosenz <oliver.prosenz@gmx.net>
2026-01-18 20:23:13 +0100

    Add File 02-xfce-23.05.sh

---
+++ 02-xfce-23.05.sh
@@ -0,0 +1,53 @@
+
+# Farbige echo-Ausgaben (nur bei echo -e)   https://gist.github.com/lomedil/41b739a74b481c4b0a47fca09f42bea3
+KF='\e[0m'			    # Keine Farbe. (Farbe zurücksetzen)
+iflil='\e[1;95m'	    # intensives fettes Lila (verschiedene Versionen)
+ifgrn='\e[1;92m'	    # intensives fettes Grün (bereits installierte Pakete)
+ifblu='\e[1;94m'	    # intensives fettes Blau (neu zu installierende Pakete)
+iftks='\e[1;96m'	    # intensives fettes Türkis (Überprüfungsmeldung) 
+itks='\e[0;96m'		    # intensives Türkis (Paketnamen)
+ifrot='\e[1;91m'	    # intensives fettes Rot (Fehler)
+ifyellow='\e[1;93m'     # intensives fettes Gelb
+
+#DISTRO='?????'             # Debian 16 Coming up
+#DISTRO='duke'              # Debian 15 Unstable/sid
+#DISTRO='forky'             # Debian 14 Testing
+#DISTRO='trixie'            # Debian 13 Stable
+#DISTRO='bookworm'  		# Debian 12 Oldstable
+
+#------------------------------------------------------------------------------------------
+
+
+
+echo ""
+echo -e ${ifyellow} '>>>>> Hier wird eine komplettinstallation von Xfce angeboten, für den Fall dass noch\n      keine Installation von Xfce durchgeführt wurde oder die Installation von Xfce\n      fehlerhaft ist und die Konfiguration zurückgesetzt werden soll.'${KF}
+
+
+
+
+echo ""
+read -p "Soll eine Komplettinstallation von Xfce durchgeführt werden? (Y/n): " xfce
+read -p "Sollen noch weitere Anwendungen speziell für den Xfce-Desktop installiert werden? (Y/n): " appsxfce
+read -p "Sollen weitere Themes und Styles für GTK-basierte Desktops wie Budgie, Gnome oder Mate installiert werden? (Y/n): " themes
+echo ""
+echo -e ${ifyellow} "Soll ein Displaymanager installiert werden und falls ja welcher Displaymanager soll verwendet werden?\n" ${KF}"(1) LightDM - Den Standarddisplaymanager von xfce, budgie, i3 u.a. verwenden\n (2) GDM     - Den Standard-Displaymanager von Gnome verwenden\n (3) sddm    - Den Simple-Desktop-Display-Manager von KDE-Plasma verwenden\n (4) Kein Displaymanager Installieren bzw. den bereits installierten nutzen"
+read -p "  " displaymanager
+echo ""
+echo -e ${ifyellow} "Soll ein bestimmter Dateibrowser installiert werden und falls ja welcher?\n" ${KF}"(1) thunar der Dateibrowser des Xfce4-Desktops\n (2) nautlius der Dateibrowser des Gnome-Desktops\n (3) dolphin der Dateibrowser des KDE-Plasma-Desktops\n (4) Alle genannten Dateibrowser installieren\n (5) Keinen Dateibrowser"
+read -p "  " files
+echo ""
+echo -e ${ifyellow} "Soll eine Kalenderapplikation installiert werden und falls ja welche?\n" ${KF}"(1) gnome-calendar des Gnome-Desktops\n (2) korganizer von KDE-Plasma\n (3) calindori für Plasma-Mobile\n (4) Alle genannten Kalenderapplikation\n (5) Keine Kalenderapplikation"
+read -p "  " kalender
+echo ""
+echo -e ${ifyellow} "Soll ein Editor installiert werden und falls ja welcher?\n" ${KF}"(1) gedit des Gnome-Desktops\n (2) kate von KDE-Plasma\n (3) geany - Kann über Plugins erweitert werden\n (4) Alle genannten Editoren installieren\n (5) Keine Editorapplikation"
+read -p "  " editor
+echo ""
+echo -e ${ifyellow} "Soll ein bestimmter Bildbetrachter installiert werden und falls ja welcher?\n" ${KF}"(1) eog der Bildbetrachter des Gnome-Desktops\n (2) gwenview der Bildbetrachter des KDE-Plasma-Desktops\n (3) lximage der Bildbetrachter des LxQT-Desktops\n (4) sxiv - ein schneller und schlanker Bildbetrachter\n (5) Alle genannten Bildbetrachter installieren\n (6) Keinen Bildbetrachter"
+read -p "  " image
+echo ""
+echo -e ${ifyellow} "Soll ein bestimmter PDF-Reader installiert werden und falls ja welcher?\n" ${KF}"(1) papers der PDF-Reader des Gnome-Desktops\n (2) okular der PDF-Reader des KDE-Plasma-Desktops\n (3) mupdf - ein schlanker und genügsamer PDF-Reader\n (4) Alle genannten PDF-Reader installieren\n (5) Keinen PDF-Reader"
+read -p "  " pdf
+echo ""
+echo -e ${ifyellow} "Soll ein Tool für Bildschirmfotos installiert werden und falls ja welches?\n" ${KF}"(1) flameshot - GUI um Fotos zu erstellen und vor dem speichern zu bearbeiten\n (2) spectacle - Das Screenshot-Tool des KDE-Plasma-Desktops\n (3) scrot - Bildschirmfotos über die Kommandozeile\n (4) xfce4-screenshooter - Das Screenshot-Tool des xfce-Desktops\n (5) Alle genannten Tools für Screenshots installieren\n (6) Kein Tool für Screenshots"
+read -p "  " screen
+
